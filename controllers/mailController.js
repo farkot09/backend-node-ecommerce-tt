8672cd0e85f3da3908a4deba2211a7f6d9852458
farkot09 <viagramo2011@gmail.com>
2024-10-28 21:36:26 -0500

    se adcciona el envio a multiples cuentas de correo

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,54 +1,111 @@
+const fs = require("fs");
+const path = require("path");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service : 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const batchSize = 3;
+const batchDelay = 10000;
+
+const sendMasiveMail = async (req, res) => {
+  const { recipients, subject, text } = req.body;
+
+  if (!recipients) {
+    return res.status(400).json({ message: "Recipients are required" });
+  }
+
+  if (!subject) {
+    return res.status(400).json({ message: "Subject is required" });
+  }
+
+  if (!text) {
+    return res.status(400).json({ message: "Text is required" });
+  }
+
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: Array.isArray(recipients) ? recipients : [recipients],
+      subject,
+      text,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: "Email sent" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const sendMasiveMailBatch = async (req, res) => {
+  const { recipients, subject, text } = req.body;
+
+  if (!recipients) {
+    return res.status(400).json({ message: "Recipients are required" });
+  }
+
+  if (!subject) {
+    return res.status(400).json({ message: "Subject is required" });
+  }
+
+  if (!text) {
+    return res.status(400).json({ message: "Text is required" });
+  }
+
+  const errors = [];
+  const responsesTransporter = [];
+
+  try {
+    const batches = [];
+
+    for (let i = 0; i < recipients.length; i += batchSize) {
+      batches.push(recipients.slice(i, i + batchSize));
     }
-})
 
-const sendMasiveEmail = async (req, res) => {
-    const { recipients, subject, text } = req.body;
-
-    if (!recipients) {
-            return res.status(400).json({
-            message: "Recipiens are required"
-        })
-    }
-
-    if (!subject) {
-        return res.status(400).json({
-            message: "subject is required"
-        })
-    }
-
-    if (!text) {
-        return res.status(400).json({
-            message: "message is required"
-        })
-    }
-
-    try {
-
-        const mailOptions = {
+    for (const batch of batches) {
+      await Promise.all(
+        batch.map(async (recipient) => {
+          const mailOptions = {
             from: process.env.EMAIL_USER,
-            to: Array.isArray(recipients) ? recipients : [recipients],
+            to: recipient,
             subject,
-            text
-        } 
+            text,
+          };
 
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({
-            message: "Email sent"
+          try {
+            let response = await transporter.sendMail(mailOptions);
+            responsesTransporter.push(response);
+            console.log(`enviado el correo a ${recipient}`);
+          } catch (error) {
+            errors.push({ recipient, error: error.message });
+            console.log(
+              `error al enviar el correo a ${recipient}: ${error.message}`
+            );
+          }
         })
-        
-    } catch (error) {
-        res.status(500).json({
-            message: "Error",
-            error
-        })
+      );
+
+      await new Promise(resolve => setTimeout(resolve, batchDelay));
     }
-}
 
-module.exports = sendMasiveEmail;
+
+    if(errors.length > 0) {
+        res.status(200).json({message: "Mensajes enviados con errores", errors});
+    }else{
+        res.status(200).json({message: "Mensajes enviados", responsesTransporter});
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { sendMasiveMail, sendMasiveMailBatch };
--- routes/routesEmail.js
+++ routes/routesEmail.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const MailController = require("../controllers/mailController");
 
-router.post("/mailMasive", MailController);
+router.post("/mailMasive", MailController.sendMasiveMail);
+router.post('/mailMasiveBatch', MailController.sendMasiveMailBatch);
 
 module.exports = router;
