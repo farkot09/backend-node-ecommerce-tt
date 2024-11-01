a10840d269c23777e317d5e748a5b5fb441e7383
farkot09 <viagramo2011@gmail.com>
2024-11-01 11:18:16 -0500

    try-to-fix-ssl-certificate-with-certbot

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+require("dotenv").config();
+
 const RoutesCustomer = require("./routes/routesCustomer");
 const RoutesProduct = require("./routes/routesProduct");
 const RoutesEmail = require("./routes/routesEmail");
 const RoutesSale = require("./routes/routesSale");
 const RoutesUser = require("./routes/routesUser");
 
-require("dotenv").config();
-const cors = require("cors");
+const app = express();
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Rutas
 app.use("/api", RoutesCustomer);
 app.use("/api", RoutesProduct);
 app.use("/api", RoutesEmail);
 app.use("/api", RoutesSale);
 app.use("/api", RoutesUser);
 
+// Cargar los certificados SSL
+const sslOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/tu-dominio.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/tu-dominio.com/fullchain.pem')
+};
 
-
+// Crear el servidor HTTPS
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor En puerto ${PORT}`);
+const httpsServer = https.createServer(sslOptions, app);
+
+// Iniciar el servidor
+httpsServer.listen(PORT, () => {
+    console.log(`Servidor HTTPS en puerto ${PORT}`);
 });
 
-module.exports = app;
+// Redirigir el tráfico HTTP a HTTPS
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
+    res.end();
+}).listen(80);
